170eb52e10c508a490b5f6f7d90f26d09eeebf20
Kale Leach <kleach1212@gmail.com>
2022-07-16 13:25:48 -0700

    commit tree 08b8fd6312e2ccb13c891bc26e871b11bd0c1ef8

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,35 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  require 'pry'
   
-  # Add routes
+  get "/messages" do 
+    messages = Message.all
+
+    messages.to_json
+  end
+
+  post "/messages" do 
+    messages = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+
+    messages.to_json
+  end
+
+  patch "/messages/:id" do 
+    messages = Message.find(params[:id])
+
+    messages.update( body: params[:body])
+
+    messages.to_json
+  end
+
+  delete "/messages/:id" do 
+    messages = Message.find(params[:id])
+    messages.destroy
+    messages.to_json
+
+  end
   
 end
